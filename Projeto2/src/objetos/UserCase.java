0388b8bf549885c669f5e7d0ce2c3fecf4f3473b
Lucas Oliveira Costa <lucasocosta@hotmail.com>
2015-04-23 13:59:48 -0300

    teste

---
+++ Projeto2/src/objetos/UserCase.java
@@ -0,0 +1,35 @@
+package objetos;
+
+import java.util.*;
+
+public class UserCase {
+	private String Descricao;
+	private List<UserCase> UC;
+	public UserCase()
+	{
+		UC = new ArrayList<UserCase>();
+	}
+	public UserCase (String Descricao)
+	{
+		this.Descricao=Descricao;
+		UC = new ArrayList<UserCase>();
+	}
+	
+	public String getDescricao() {
+		return Descricao;
+	}
+	public void setDescricao(String descricao) {
+		Descricao = descricao;
+	}
+	public List<UserCase> getUserCase() {
+		return UC;
+	}
+	public void addUserCase(UserCase c)
+	{
+		UC.add(c);
+	}
+	public UserCase getUserCaseAt(int i)
+	{
+		return UC.get(i);
+	}
+}
