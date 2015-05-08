2e9c1a4e3e9f3d0f8073f93fd4eab024692734e0
Júlio César Teixeira de Oliveira <juliocto2011@gmail.com>
2015-05-08 08:35:00 -0300

    Implementação do Salvar via Xstream.

--- Projeto2/src/control/Salvar.java
+++ Projeto2/src/control/Salvar.java
@@ -5,17 +5,32 @@
  */
 package control;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 import entidade.Modelo;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 /**
  *
  * @author Lucas
  */
 public class Salvar {
-    public static int salvar(Modelo model, File caminho)
+    public static int salvar(Modelo model, File caminho) throws FileNotFoundException
     {
+        //Incluir uma instancia para o Xstream
+        XStream xstream = new XStream(new DomDriver());        
+        String xml1 = xstream.toXML(model);        
         
+        //Conferir no terminal o xml gerado
+        System.out.println(xml1);
+        
+        //Salvar o modelo no formato xml
+        try (PrintWriter writer = new PrintWriter("arquivo.xml")) {
+            writer.println(xml1);
+            writer.close();
+        }
         return 0;
     }
 }
