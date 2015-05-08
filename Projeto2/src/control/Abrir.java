a57dbafae33e7c34f188055809700d0572e87c9d
Júlio César Teixeira de Oliveira <juliocto2011@gmail.com>
2015-05-08 08:47:54 -0300

    Implementação do Abrir via Xstream.

--- Projeto2/src/control/Abrir.java
+++ Projeto2/src/control/Abrir.java
@@ -6,14 +6,22 @@
 package control;
 
 import java.io.File;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 /**
  *
  * @author Lucas
  */
 import entidade.Modelo;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Abrir {
-    public static Modelo abrir(File arquivo)
+    public static Modelo abrir(File arquivo) throws FileNotFoundException
     {
-        return new Modelo();
+        XStream xstream = new XStream(new DomDriver()); 
+        String content = new Scanner(new File("arquivo.xml")).useDelimiter("\\Z").next();
+        Modelo modeloArquivo = (Modelo)xstream.fromXML(content);
+        System.out.println(content);
+        return  modeloArquivo;
     }
 }
