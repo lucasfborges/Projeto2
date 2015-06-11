fcdaef65f9c2f521b74aabee230444803b3ff3c9
Lucas Oliveira Costa <lucasocosta@hotmail.com>
2015-06-11 14:08:12 -0300

    main

---
+++ Projeto2/src/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import gui.*;
+
+/**
+ *
+ * @author Lucas
+ */
+public class Main
+{
+
+    public static void main(String args[]) {
+
+                new TextEditor().setVisible(true);
+    }
+}
