8cf0e1d95eb175d95955d6eae64d9f2babc9cac8
Lucas Oliveira Costa <lucasocosta@hotmail.com>
2015-06-11 14:57:11 -0300

    getter e setter do texto no modelo

--- Projeto2/src/entidade/Modelo.java
+++ Projeto2/src/entidade/Modelo.java
@@ -13,6 +13,22 @@ import java.util.*;
 
 public class Modelo {
     private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     private String name;
     
     
