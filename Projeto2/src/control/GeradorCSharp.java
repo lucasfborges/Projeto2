6977bab6e3e5a2d39dedddf9cead7cf3fb1886bd
huras <hurast@gmail.com>
2015-07-01 00:44:09 -0300

    Gerador de CSharp quase completo
    Gerador dos menus principais somente,
    foreachs estão funcionando corretamente ?

--- Projeto2/src/control/GeradorCSharp.java
+++ Projeto2/src/control/GeradorCSharp.java
@@ -7,6 +7,18 @@ package control;
 
 import entidade.Modelo;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.exception.MethodInvocationException;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
 
 /**
  *
@@ -14,7 +26,42 @@ import java.io.File;
  */
 public class GeradorCSharp implements Gerador {
     static int gerar(Modelo model, File caminho) {
-        return Gerador.ERRO;
+        Properties p = new Properties();
+        p.setProperty("file.resource.loader.path", new File("").getAbsolutePath() + "/src/templatesCSharp/");
+        Velocity.init(p);
+
+        VelocityContext context = new VelocityContext();
+
+        context.put("model", model);
+
+        Template template = null;
+
+        try {
+            template = Velocity.getTemplate("teste.vm");
+        } catch (ResourceNotFoundException rnfe) {
+            // couldn't find the template
+        } catch (ParseErrorException pee) {
+            // syntax error: problem parsing the template
+        } catch (MethodInvocationException mie) {
+            // something invoked in the template
+            // threw an exception
+        } catch (Exception e) {
+        }
+
+        StringWriter sw = new StringWriter();
+
+        template.merge(context, sw);
+
+        try {
+            FileWriter fw = new FileWriter(caminho);
+            fw.write(sw.toString());
+            fw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Gerador.class.getName()).log(Level.SEVERE, null, ex);
+            return Gerador.ERRO;            
+        }
+
+        return Gerador.SUCESSO;
     }
 }
 
